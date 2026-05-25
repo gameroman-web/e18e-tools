ef41cb1d8f284e1d62d2b76a504aad0ab499f330
Roman <dev@rman.dev>
2026-05-25 15:08:24 +0100

    wip

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,13 @@
-import { registryUrl } from "./constants";
+import { liveRegistryUrl, npmRegistryBaseUrl, registryUrl } from "./constants";
 import { updateProgress } from "./ui";
 import { hash } from "./util";
 
-async function cachedFetch<T>(url: string, options: RequestInit = {}) {
+async function cachedFetch<T, P = T>(url: string, options: RequestInit = {}) {
   const hashKey = `fetch:${hash(url + (options.body || ""))}`;
   const cached = localStorage.getItem(hashKey);
 
   if (cached) {
-    const { data, expiry }: { data: T; expiry: number } = JSON.parse(cached);
+    const { data, expiry }: { data: P; expiry: number } = JSON.parse(cached);
     if (Date.now() < expiry) return { data, isCached: true as const };
     localStorage.removeItem(hashKey);
   }
@@ -19,7 +19,7 @@ async function cachedFetch<T>(url: string, options: RequestInit = {}) {
   return {
     data: rawData,
     isCached: false as const,
-    commit(processedData: T, ttlMinutes = 10) {
+    commit(processedData: P, ttlMinutes = 10) {
       try {
         const entry = {
           data: processedData,
@@ -36,19 +36,30 @@ async function cachedFetch<T>(url: string, options: RequestInit = {}) {
   };
 }
 
-async function fetchAllStats(names) {
-  const combinedStats = {};
+interface DownloadsStatsRow {
+  id: string;
+  key: string;
+  value: number;
+}
+
+interface DownloadsStats {
+  total_rows: number;
+  offset: number;
+  rows: DownloadsStatsRow[];
+}
+
+async function fetchAllStats(names: string[]) {
+  const combinedStats: Record<string, number> = {};
 
   updateProgress(40, `Fetching stats for ${names.length} packages...`);
 
-  const { data } = await cachedFetch(
-    `${registryUrl}/_design/downloads/_view/downloads`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ keys: names }),
-    },
-  );
+  const url = `${registryUrl}/_design/downloads/_view/downloads` as const;
+
+  const { data } = await cachedFetch<DownloadsStats>(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ keys: names }),
+  });
 
   data.rows.forEach((r) => {
     combinedStats[r.key] = r.value;
@@ -58,4 +69,82 @@ async function fetchAllStats(names) {
   return combinedStats;
 }
 
-export { cachedFetch, fetchAllStats };
+interface PackageDist {
+  unpackedSize: number;
+  size: number;
+}
+
+interface PackageInfo {
+  dist: PackageDist;
+}
+
+async function getBasePackageSize(name: string): Promise<number> {
+  const url = `${npmRegistryBaseUrl}/${name}/latest` as const;
+  const result = await cachedFetch<PackageInfo>(url);
+  if (!result.isCached) {
+    result.commit(result.data);
+  }
+  return result.data.dist?.size || 0;
+}
+
+interface DevDependentsRow {
+  id: string;
+  key: string;
+  value: string & { version: undefined };
+}
+
+interface DependentsRow {
+  id: string;
+  key: string;
+  value: { name: string; version: string };
+}
+
+interface Dependents {
+  total_rows: number;
+  offset: number;
+  rows: DependentsRow[] | DevDependentsRow[];
+}
+
+interface ProcessedDependent {
+  n: string;
+  v: string;
+  d: number;
+}
+
+async function getSortedDependents(
+  packageName: string,
+  isDev?: boolean,
+): Promise<ProcessedDependent[]> {
+  const view = isDev ? "dev-dependencies" : "dependents2";
+  const url =
+    `${liveRegistryUrl}/_design/dependents/_view/${view}?key="${packageName}"` as const;
+
+  const { data, isCached, commit } = await cachedFetch<
+    Dependents,
+    ProcessedDependent[]
+  >(url);
+
+  if (isCached) return data;
+
+  updateProgress(30, "Analyzing dependent list...");
+
+  const allNames = data.rows.map((r) => r.id);
+  const allStats = await fetchAllStats(allNames);
+
+  const MAX_DEPENDENTS = 3000;
+
+  const processed = data.rows
+    .map((r) => ({
+      n: r.id,
+      v: r.value?.version || "",
+      d: allStats[r.id] || 0,
+    }))
+    .sort((a, b) => b.d - a.d)
+    .slice(0, MAX_DEPENDENTS);
+
+  commit(processed);
+
+  return processed;
+}
+
+export { cachedFetch, fetchAllStats, getBasePackageSize, getSortedDependents };
