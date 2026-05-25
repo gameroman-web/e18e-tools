cdbd6adfb8e2dd376df88e7b20a01137bf8c611a
Roman <dev@rman.dev>
2026-05-25 14:10:57 +0100

    wip

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+const npmRegistryBaseUrl = "https://registry.npmmirror.com";
+const registryUrl = "https://npm.devminer.xyz/registry";
+const liveRegistryUrl = "https://npm.devminer.xyz/live_registry";
+
+export { liveRegistryUrl, npmRegistryBaseUrl, registryUrl };
